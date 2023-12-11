a24ad90622aedbe85ab0477364790eac147b2b84
Oleg Fursa <olegfursa93@hotmail.com>
2023-12-11 21:32:31 +0000

    Create taskmanager database

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 import os
 from taskmanager import app
 
-
-if __name__ == "__main__":
-    app.run(
-        host=os.environ.get("IP"),
-        port=int(os.environ.get("PORT")),
-        debug=os.environ.get("DEBUG")
-    )
+def test_connection(self):
+    with app.app_context():
+    if __name__ == "__main__":
+        app.run(
+            host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=os.environ.get("DEBUG")
+        )
