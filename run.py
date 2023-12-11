5cbbc657d60a2162f05340b44ef3558df49af3ee
Oleg Fursa <olegfursa93@hotmail.com>
2023-12-11 22:26:36 +0000

    Create base template

--- run.py
+++ run.py
@@ -1,11 +1,10 @@
 import os
 from taskmanager import app
 
-def test_connection(self):
-    with app.app_context():
-    if __name__ == "__main__":
-        app.run(
-            host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
-            debug=os.environ.get("DEBUG")
-        )
+
+if __name__ == "__main__":
+    app.run(
+    host=os.environ.get("IP"),
+    port=int(os.environ.get("PORT")),
+    debug=os.environ.get("DEBUG")
+    )
